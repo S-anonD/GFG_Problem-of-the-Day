f35e1dbb77a72921ebffccd34da9424b86c89e91
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2024-11-16 22:28:28 +0530

    Add files via upload

---
+++ gfg_16_11_24.py
@@ -0,0 +1,11 @@
+# 16/11/24
+# Move All Zeroes to End
+
+class Solution:
+	def pushZerosToEnd(self,arr):
+	    count = 0
+	    for i in range(len(arr)):
+	        if arr[i] != 0:
+	            arr[i], arr[count] = arr[count], arr[i]
+	            count += 1
+	    return
