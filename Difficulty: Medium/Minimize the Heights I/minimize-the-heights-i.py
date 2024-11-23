26a67240c540ebbd2026cd5c7a9956bbed7965c0
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2024-11-23 21:29:51 +0530

    Update minimize-the-heights-i.py

--- Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.py
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.py
@@ -6,10 +6,6 @@ class Solution:
         arr.sort()
         res = arr[n-1] - arr[0]
         for i in range(1, n):
-            """
-            if arr[i] - k < 0:
-                continue
-            """
             minH = min(arr[0]+k, arr[i]-k)
             maxH = max(arr[i-1]+k, arr[n-1]-k)
             res = min(res, maxH-minH)
