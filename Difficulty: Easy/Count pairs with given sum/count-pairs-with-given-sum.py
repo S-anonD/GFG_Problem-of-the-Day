849468e853a2c0aff4df801df7e2759301920b5f
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2024-12-27 21:50:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.py
@@ -0,0 +1,42 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    #Complete the below function
+    def countPairs(self,arr, target):
+        count = 0
+        freq = {}
+        for i in range(len(arr)):
+            if (target - arr[i]) in freq:
+                count += freq[target - arr[i]]
+            freq[arr[i]] = freq.get(arr[i], 0) + 1
+        return count
+        #Your code here
+
+#{ 
+ # Driver Code Starts.
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        A = [int(x) for x in input().strip().split()]
+
+        k = int(input())
+        ob = Solution()
+        print(ob.countPairs(A, k))
+        print('~')
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
