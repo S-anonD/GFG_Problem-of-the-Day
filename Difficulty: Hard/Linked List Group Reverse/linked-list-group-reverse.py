b2908ee15c419adebe9ab405c5316cb080ab5024
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-01-21 00:17:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.py
@@ -0,0 +1,79 @@
+"""Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+"""
+class Solution:
+    def reverseKGroup(self, head, k):
+        if not head or k == 1:
+            return head
+        st = []
+        curr = head
+        prev = None
+        while curr is not None:
+            count = 0
+            while curr is not None and count < k:
+                st.append(curr)
+                curr = curr.next
+                count += 1
+            while len(st) > 0:
+                if prev is None:
+                    prev = st.pop()
+                    head = prev
+                else:
+                    prev.next = st.pop()
+                    prev = prev.next
+        prev.next = None
+        return head
+        # Code here
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+if __name__ == '__main__':
+    t = int(input())  # Number of test cases
+    while t > 0:
+        llist = LinkedList()
+
+        # Read list values and push them to the LinkedList
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+
+        k = int(input())  # Size of the group for reversal
+        ob = Solution()
+        new_head = ob.reverseKGroup(llist.head, k)
+        llist.head = new_head
+        llist.printList()  # Print the modified linked list
+        t -= 1
+
+        print("~")
+
+# } Driver Code Ends
