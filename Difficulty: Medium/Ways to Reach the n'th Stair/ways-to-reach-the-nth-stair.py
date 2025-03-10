34c9db99035990488010424467f5085e6663e3de
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-03-11 00:08:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.py
@@ -0,0 +1,33 @@
+
+class Solution:
+    def countWays(self, n):
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+sys.setrecursionlimit(10**6)
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        m = int(input())
+        ob = Solution()
+        print(ob.countWays(m))
+
+        print("~")
+
+# } Driver Code Ends
