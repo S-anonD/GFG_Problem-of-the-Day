f1d369f4e37c34fd34cc49e4e91937de2f1c7479
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-03-13 01:01:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    # Function to return max value that can be put in knapsack of capacity.
+    def knapsack(self, W, val, wt):
+        n = len(wt)
+        dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+        for i in range(n + 1):
+            for j in range(W + 1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
+                else:
+                    pick = 0
+                    if wt[i - 1] <= j:
+                        pick = val[i - 1] + dp[i - 1][j - wt[i - 1]]
+                    notPick = dp[i - 1][j]
+                    dp[i][j] = max(pick, notPick)
+        return dp[n][W]
+        # code here
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    test_cases = int(input())
+    for _ in range(test_cases):
+        capacity = int(input())
+        values = list(map(int, input().strip().split()))
+        weights = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.knapsack(capacity, values, weights))
+        print("~")
+# } Driver Code Ends
