88756544036756d66d09c2697ba3cf1c95f4aaeb
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-06-09 19:45:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isDeadEnd(self, root):
+        return self.dfs(root, 1, float('inf'))
+    
+    def dfs(self, root, mini, maxi):
+        if root is None:
+            return False
+        if root.left is None and root.right is None and mini == maxi:
+            return True
+        return self.dfs(root.left, mini, root.data - 1) or self.dfs(root.right, root.data + 1, maxi)
+        # Code here
