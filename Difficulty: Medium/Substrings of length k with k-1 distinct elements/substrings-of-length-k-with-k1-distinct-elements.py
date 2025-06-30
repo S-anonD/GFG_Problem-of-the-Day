89a959f6c86c83cf2075d5aa86d35e0f2ce835ba
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-07-01 00:32:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings of length k with k-1 distinct elements/substrings-of-length-k-with-k1-distinct-elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def substrCount(self, s, k):
+        if k > len(s):
+            return 0
+        n = len(s)
+        cnt = [0] * 26
+        ans = 0
+        for i in range(k - 1):
+            cnt[ord(s[i]) - ord('a')] += 1
+        for i in range(k - 1, n):
+            cnt[ord(s[i]) - ord('a')] += 1
+            distinctCount = sum(1 for x in cnt if x > 0)
+            if distinctCount == k - 1:
+                ans += 1
+            cnt[ord(s[i - k + 1]) - ord('a')] -= 1
+        return ans
+        # code here
+        
