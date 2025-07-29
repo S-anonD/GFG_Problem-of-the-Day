8de1ae7443d8fdae40a54677020f436229b82db9
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-07-30 00:11:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -1,51 +1,15 @@
-#User function Template for python3
-
 class Solution:
-    def countSubarrays(self, arr, k):
+    def cntSubarrays(self, arr, k):
         prefixSums = {}
-        count, csm = 0, 0
+        res = 0
+        currSum = 0
         for val in arr:
-            csm += val
-            if csm == k:
-                count += 1
-            if csm - k in prefixSums:
-                count += prefixSums[csm - k]
-            prefixSums[csm] = prefixSums.get(csm, 0) + 1
-        return count
+            currSum += val
+            if currSum == k:
+                res += 1
+            if currSum - k in prefixSums:
+                res += prefixSums[currSum - k]
+            prefixSums[currSum] = prefixSums.get(currSum, 0) + 1
+        return res
         # code here
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-from collections import deque
-
-#Contributed by : Nagendra Jha
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        k = int(input())
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        res = ob.countSubarrays(arr, k)
-        print(res)
-        print("~")
-
-# } Driver Code Ends
+        
