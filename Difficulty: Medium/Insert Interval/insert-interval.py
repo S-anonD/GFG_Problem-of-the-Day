53d824c25d93408e647b3c52ee0509858f34721a
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-08-15 00:46:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.py
+++ Difficulty: Medium/Insert Interval/insert-interval.py
@@ -1,14 +1,8 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-#User function Template for python3
-
 class Solution:
     def insertInterval(self, intervals, newInterval):
         res = []
-        i, n = 0, len(intervals)
+        i = 0
+        n = len(intervals)
         while i < n and intervals[i][1] < newInterval[0]:
             res.append(intervals[i])
             i += 1
@@ -22,24 +16,4 @@ class Solution:
             i += 1
         return res
         # Code here
-
-#{ 
- # Driver Code Starts.
-if __name__ == '__main__': 
-    t = int(input ())
-    for _ in range (t):
-        N = int(input())
-        intervals = [list(map(int, input().split())) for i in range(N)]
-        newEvent = list(map(int, input().split()))
-        ob = Solution()
-        res = ob.insertInterval(intervals, newEvent)
-        print('[', end = '')
-        for i in range(len(res)):
-            print('[', end = '')
-            print(str(res[i][0])+','+str(res[i][1]), end = '')
-            print(']', end = '')
-            if i < len(res)-1:
-                print(',', end='')
-        print(']')
-        print("~")
-# } Driver Code Ends
+        
