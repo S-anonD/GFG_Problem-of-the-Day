e6d71945f894021b3758f55002e7c724756718d8
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-08-23 00:59:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -1,11 +1,7 @@
 class Solution:
-    
-    #Function to find minimum number of pages.
     def findPages(self, arr, k):
         if k > len(arr):
             return -1
-        if k == 1:
-            return sum(arr)
         lo = max(arr)
         hi = sum(arr)
         res = -1
@@ -17,9 +13,11 @@ class Solution:
             else:
                 lo = mid + 1
         return res
-    
+        # code here
+
     def check(self, arr, k, pageLimit):
-        cnt, pageSum = 1, 0
+        cnt = 1
+        pageSum = 0
         for pages in arr:
             if pageSum + pages > pageLimit:
                 cnt += 1
@@ -27,25 +25,3 @@ class Solution:
             else:
                 pageSum += pages
         return cnt <= k
-        #code here
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import bisect
-#Main
-if __name__ == '__main__':
-    t = int(input())
-    while t:
-        t -= 1
-        A = [int(x) for x in input().strip().split()]
-        nd = [int(x) for x in input().strip().split()]
-        D = nd[0]
-        ob = Solution()
-        ans = ob.findPages(A, D)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
