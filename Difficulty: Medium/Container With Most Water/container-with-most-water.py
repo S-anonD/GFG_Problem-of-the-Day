aa4512ec401ffce6ca24f6ce912b4ea246b96fe0
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-08-31 00:59:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.py
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -1,39 +1,15 @@
-
 class Solution:
     def maxWater(self, arr):
-        l, r = 0, len(arr) - 1
-        quant = 0
-        while l < r:
-            water = min(arr[l], arr[r]) * (r - l)
-            quant = max(quant, water)
-            if arr[l] < arr[r]:
-                l += 1
+        left = 0
+        right = len(arr) - 1
+        res = 0
+        while left < right:
+            water = min(arr[left], arr[right]) * (right - left)
+            res = max(res, water)
+            if arr[left] < arr[right]:
+                left += 1
             else:
-                r -= 1
-        return quant
+                right -= 1
+        return res
         # code here
-
-
-#{ 
- # Driver Code Starts
-#Initial template for Python 3
-
-import math
-
-
-def main():
-    t = int(input())
-    while (t > 0):
-
-        arr = [int(x) for x in input().strip().split()]
-        obj = Solution()
-        print(obj.maxWater(arr))
-
-        t -= 1
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+        
