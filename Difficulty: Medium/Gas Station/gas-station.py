bcc412d5d7f2eb7f0ee8ddda70221be8487c7cf1
Anondita Dutta <122111426+S-anonD@users.noreply.github.com>
2025-09-14 00:39:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.py
+++ Difficulty: Medium/Gas Station/gas-station.py
@@ -1,38 +1,16 @@
 class Solution:
     def startStation(self, gas, cost):
         n = len(gas)
-        startidx = 0
-        currgas = 0
+        totalGas = 0
+        currGas = 0
+        startIdx = 0
         for i in range(n):
-            currgas += (gas[i] - cost[i])
-            if currgas < 0:
-                startidx = i + 1
-                currgas = 0
-        currgas = 0
-        for i in range(n):
-            idx = (i + startidx) % n
-            currgas += (gas[idx] - cost[idx])
-            if currgas < 0:
-                return -1
-        return startidx
-        # Your code here
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import io
-import sys
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        gas = list(map(int, input().strip().split()))
-        cost = list(map(int, input().strip().split()))
-        ob = Solution()
-        ans = ob.startStation(gas, cost)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
+            currGas += gas[i] - cost[i]
+            totalGas += gas[i] - cost[i]
+            if currGas < 0:
+                currGas = 0
+                startIdx = i + 1
+        if totalGas < 0:
+            return -1
+        return startIdx
+        #  code here
